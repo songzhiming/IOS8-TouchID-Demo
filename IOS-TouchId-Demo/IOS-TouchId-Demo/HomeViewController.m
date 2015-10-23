b7138d04ff2d7102af11878d4ecd1173a8884772
宋志明 <15117951262@163.com>
2015-10-23 15:28:51 +0800

    修改

--- IOS-TouchId-Demo/IOS-TouchId-Demo/HomeViewController.m
+++ IOS-TouchId-Demo/IOS-TouchId-Demo/HomeViewController.m
@@ -25,23 +25,25 @@
     // Dispose of any resources that can be recreated.
 }
 - (IBAction)onclickButton:(id)sender {
-
+    //新建LAContext实例
     LAContext  *authenticationContext= [[LAContext alloc]init];
     NSError *error;
+    //1:检查Touch ID 是否可用
     if ([authenticationContext canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics error:&error]) {
         NSLog(@"touchId 可用");
+        //2:执行认证策略
         [authenticationContext evaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics localizedReason:@"需要验证您的指纹来确认您的身份信息" reply:^(BOOL success, NSError * _Nullable error) {
             if (success) {
                 NSLog(@"通过了Touch Id指纹验证");
             }else{
                 NSLog(@"error===%@",error);
-                NSLog(@"code====%ld",error.code);
+                NSLog(@"code====%d",error.code);
                  NSLog(@"errorStr ======%@",[error.userInfo objectForKey:NSLocalizedDescriptionKey]);
-                if (error.code == -1) {//点击了取消按钮
+                if (error.code == -2) {//点击了取消按钮
                     NSLog(@"点击了取消按钮");
-                }else if (error.code == -2){//点输入密码按钮
+                }else if (error.code == -3){//点输入密码按钮
                     NSLog(@"点输入密码按钮");
-                }else if (error.code == -3){//连续三次指纹识别错误
+                }else if (error.code == -1){//连续三次指纹识别错误
                     NSLog(@"连续三次指纹识别错误");
                 }else if (error.code == -4){//按下电源键
                     NSLog(@"按下电源键");
@@ -53,6 +55,7 @@
         }];
     }else{
         //todo goto 输入密码页面
+        NSLog(@"error====%@",error);
         NSLog(@"抱歉，touchId 不可用");
     }
 }
